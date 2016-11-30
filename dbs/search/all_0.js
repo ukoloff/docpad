308b20f74f765bb52e8c7cc32b7fdea13c4f87ac
Travis CI Deployer <deployer@travis-ci.org>
2016-11-30 13:13:51 +0000

    ac9b3a9 Merge branch 'doxygen'

---
+++ dbs/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['a',['a',['../structdbs_1_1_span.html#a71429bf8897aafc2ac492f7d6149ab9c',1,'dbs::Span']]],
+  ['at',['at',['../structdbs_1_1_span.html#a1e9dfeeb5a1455b1b3cdd763e0fd7718',1,'dbs::Span']]]
+];
---
+++ dbs/structdbs_1_1_path.js
@@ -0,0 +1,9 @@
+var structdbs_1_1_path =
+[
+    [ "closed", "structdbs_1_1_path.html#a75c40ddab697f8aca6db939fcb0f7a96", null ],
+    [ "dxf", "structdbs_1_1_path.html#a2792a395f6c2a7a811e14af542db4f29", null ],
+    [ "json", "structdbs_1_1_path.html#a9986a0812dd2d0a48e69cd87c2673c94", null ],
+    [ "reverse", "structdbs_1_1_path.html#ae095dbd9e5ea34514e3db9a8f26a4b11", null ],
+    [ "yaml", "structdbs_1_1_path.html#aa900b03a5ab8ed0bf1ab966e98224fda", null ],
+    [ "nodes", "structdbs_1_1_path.html#a9499191dffdf7d65bf22eaebeb06da09", null ]
+];
